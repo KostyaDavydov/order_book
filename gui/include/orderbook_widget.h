29d2d16ff8d55c7cc87c62888707ebdf7b396a82
KostyaDavydov <davydov.e.konstantin@gmail.com>
2026-04-09 15:18:25 +0300

    Implement trading simulation process and its visualization
    The simulation runs in a separate thread and is controlled
    via menu buttons in the main window.
    
    **Changes:**
    - added `TradingSimulationThread` class that operates on
      an order book that it keeps as its field:
        > `m_book`: instance of the OrderBook class;
        > `m_finish`: `std::atomic<bool>` flag for thread-safe
          stop signalling;
        > `run()`: main simulation loop. Each action performs
          a randomly generated action on the order book (add,
          delete, modify, execute). The first 10 iterations
          pre-populate the book with initial asks and bids.
          Every action emits a `book_updated()` signal to
          refresh the visualization in the main window;
        > `finish_request()`: called to request thread
          termination - sets `m_finish` flag causing the
          simulation loop to exit cleanly;
    - added some members to the OrderBookWidget class to
      manipulate a separate simulation process thread:
        > `mp_tradingSimuThread`: pointer to the simulation
          thread instance;
        > `create_configure_start_thread()`: creates, configures,
          and launches the simulation thread;
        > `finish_simulation()`: requests the simulation thread
          to finish;
        > `update_book_presentation()`: updates the book
          presentation in the main window (connected to
          `book_updated()` signal);
    - added a menu bar to the main window with buttons to start
      and finish the simulation process

--- gui/include/orderbook_widget.h
+++ gui/include/orderbook_widget.h
@@ -9,7 +9,39 @@
 #include <QWidget>
 #include <QThread>
 
+#include <atomic>
+
 //=================================================
+//=================================================
+
+// The class describes a thread in which simulation process runs
+class TradingSimulationThread : public QThread
+{
+    Q_OBJECT
+
+private:
+    // Order book
+    OrderBook m_book;
+
+    // Flag that is used to finish simulation
+    std::atomic<bool> m_finish {false};
+
+    // Main thread function (simulation process)
+    void run() override;
+
+public slots:
+    // Request to finish simulation
+    void finish_request();
+
+signals:
+    // Signal provides all necessary information to update
+    // the order book presentation in the main window
+    void book_updated(const PriceLevelsVector & askLvlsSnapshot,
+                      const PriceLevelsVector & bidLvlsSnapshot,
+                      double marketPrice,
+                      double spread);
+};
+
 //=================================================
 
 class OrderBookWidget : public QMainWindow
@@ -25,11 +57,27 @@ private:
 
     QCustomPlot * mp_DOMCustomPlot;
 
-// Other fields
-    bool m_pauseSimulation;
-    double m_marketPrice;
+// Menu actions
+    QAction * mp_startSimuAct;
+    QAction * mp_stopSimuAct;
+
+// Thread for trading simulation process
+    TradingSimulationThread * mp_tradingSimuThread;
+
+    // Create, configure, and start the trading simulation thread
+    void create_configure_start_thread();
 
 public:
     explicit OrderBookWidget(QWidget * parent = nullptr);
     ~OrderBookWidget();
+
+signals:
+    void finish_simulation();
+
+public slots:
+    // Update the order book presentation in the main window
+    void update_book_presentation(const PriceLevelsVector & askLvlsSnapshot,
+                                  const PriceLevelsVector & bidLvlsSnapshot,
+                                  double marketPrice,
+                                  double spread);
 };
--- gui/src/orderbook_widget.cpp
+++ gui/src/orderbook_widget.cpp
@@ -2,14 +2,108 @@
 
 #include <QBoxLayout>
 #include <QSplitter>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+
+#include <random>
+
+Q_DECLARE_METATYPE(PriceLevelsVector)
+
+//=================================================
+//=================================================
+
+void TradingSimulationThread::finish_request()
+{
+    m_finish = true;
+}
+
+//=================================================
+
+void TradingSimulationThread::run()
+{
+    // Codes of the actions over the order book
+    enum class Action {ADD = 0, DLT = 1, MDF = 2, EXEC = 3};
+
+    // Each action as well as parameters for actions are generated randomly
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // ADD - 0; DELETE - 1; MODIFY - 2; EXECUTE - 3
+    std::uniform_int_distribution<> actionDist(0, 3);
+
+    // ASK - 0; BID - 1
+    std::uniform_int_distribution<> typeDist(0, 1);
+
+    // Order price and volume
+    std::uniform_real_distribution<> priceDist(1, 100);
+    std::uniform_int_distribution<> volumeDist(1, 10);
+
+    for (OrderID currId = 0; currId < SIZE_MAX && m_finish == false;)
+    {
+        if (currId < 5) // initial addition of five asks
+        {
+            m_book.add_order(currId++, 100.0 - currId * 10, volumeDist(gen), OrderType::ASK);
+        }
+        else if (currId < 10) // initial addition of five bids
+        {
+            m_book.add_order(currId++, 100.0 - currId * 10, volumeDist(gen), OrderType::BID);
+        }
+        else // subsequent iterations
+        {
+            // Generate the action to perform over the order book
+            auto action = static_cast<Action>(actionDist(gen));
+
+            switch (action)
+            {
+            case Action::ADD:
+            {
+                m_book.add_order(currId++,
+                                 priceDist(gen),
+                                 volumeDist(gen),
+                                 static_cast<OrderType>(typeDist(gen)));
+                break;
+            }
+            case Action::DLT:
+            {
+                if (currId == 0) // no orders were added yet
+                    continue;
+                std::uniform_int_distribution<> idDist(0, currId - 1);
+                m_book.delete_order(idDist(gen));
+                break;
+            }
+            case Action::MDF:
+            {
+                if (currId == 0) // no orders were added yet
+                    continue;
+                std::uniform_int_distribution<> idDist(0, currId - 1);
+                m_book.modify_order(idDist(gen), priceDist(gen), volumeDist(gen));
+                break;
+            }
+            case Action::EXEC:
+            {
+                m_book.execute(static_cast<OrderType>(typeDist(gen)), volumeDist(gen));
+                break;
+            }
+            }
+        }
+
+        // Notify the main thread that the book was updated
+        emit book_updated(m_book.price_levels_for_type(OrderType::ASK),
+                          m_book.price_levels_for_type(OrderType::BID),
+                          m_book.market_price(),
+                          m_book.spread());
+
+        // Little delay before the next action
+        QThread::sleep(1);
+    }
+}
 
 //=================================================
 //=================================================
 
 OrderBookWidget::OrderBookWidget(QWidget * parent)
     : QMainWindow{parent}
-    , m_pauseSimulation{true}
-    , m_marketPrice{0.0}
 {
     // Window title (on the frame)
     setWindowTitle("Order book");
@@ -54,6 +148,38 @@ OrderBookWidget::OrderBookWidget(QWidget * parent)
     mainSplitter->addWidget(mp_DOMCustomPlot);
     mainSplitter->addWidget(bookPartWgt);
 
+    // Register the complex type so that objects of that type are able
+    // to be transferred using the signal/slot mechanism
+    qRegisterMetaType<PriceLevelsVector>();
+
+    // Create and configure a menu bar
+
+    auto menuBar = new QMenuBar;
+    setMenuBar(menuBar);
+
+    auto launchMenu = menuBar->addMenu("&Launch");
+    auto simulationMenu = launchMenu->addMenu("Simulation");
+
+    mp_startSimuAct = simulationMenu->addAction("Start Simulation", this,
+                                                [this] ()
+                                                {
+                                                    this->mp_startSimuAct->setDisabled(true);
+                                                    this->mp_stopSimuAct->setEnabled(true);
+                                                    this->create_configure_start_thread();
+                                                });
+
+    mp_stopSimuAct = simulationMenu->addAction("Stop Simulation", this,
+                                               [this] ()
+                                               {
+                                                   this->mp_startSimuAct->setEnabled(true);
+                                                   this->mp_stopSimuAct->setDisabled(true);
+                                                   emit finish_simulation();
+                                                   mp_tradingSimuThread->wait();
+                                               });
+
+    mp_startSimuAct->setEnabled(true);
+    mp_stopSimuAct->setDisabled(true);
+
     // Last display settings
 
     setCentralWidget(mainSplitter);
@@ -66,4 +192,58 @@ OrderBookWidget::OrderBookWidget(QWidget * parent)
 
 OrderBookWidget::~OrderBookWidget()
 {
+    // Stop the simulation thread and wait until it completely terminates
+    emit finish_simulation();
+    mp_tradingSimuThread->wait();
+}
+
+//=================================================
+
+void OrderBookWidget::create_configure_start_thread()
+{
+    mp_tradingSimuThread = new TradingSimulationThread{};
+    connect(this, &OrderBookWidget::finish_simulation, mp_tradingSimuThread, &TradingSimulationThread::finish_request);
+    connect(mp_tradingSimuThread, &QThread::finished, mp_tradingSimuThread, &QObject::deleteLater);
+    connect(mp_tradingSimuThread, &TradingSimulationThread::book_updated, this, &OrderBookWidget::update_book_presentation);
+    mp_tradingSimuThread->start();
+}
+
+//=================================================
+
+void OrderBookWidget::update_book_presentation(const PriceLevelsVector & askLvlsSnapshot,
+                                               const PriceLevelsVector & bidLvlsSnapshot,
+                                               double marketPrice,
+                                               double spread)
+{
+    // Fill the asks table
+    mp_asksTblWgt->setRowCount(askLvlsSnapshot.size());
+    for (auto i = 0; i < askLvlsSnapshot.size(); ++i)
+    {
+        mp_asksTblWgt->setItem(askLvlsSnapshot.size() - 1 - i, 0, new QTableWidgetItem(QString::number(askLvlsSnapshot[i].first)));
+        mp_asksTblWgt->setItem(askLvlsSnapshot.size() - 1 - i, 1, new QTableWidgetItem(QString::number(askLvlsSnapshot[i].second)));
+    }
+    mp_asksTblWgt->scrollToBottom(); // asks with the lowest prices have to be seen when book updates
+
+    // Fill the bids table
+    mp_bidsTblWgt->setRowCount(bidLvlsSnapshot.size());
+    for (auto i = 0; i < bidLvlsSnapshot.size(); ++i)
+    {
+        mp_bidsTblWgt->setItem(i, 0, new QTableWidgetItem(QString::number(bidLvlsSnapshot[i].first)));
+        mp_bidsTblWgt->setItem(i, 1, new QTableWidgetItem(QString::number(bidLvlsSnapshot[i].second)));
+    }
+    mp_bidsTblWgt->scrollToTop(); // bids with the highest prices have to be seen when book updates
+
+    // Set the current market price
+    if (marketPrice == -1.0) // if there weren't any transactions yet
+        mp_marketPriceLbl->setText("Market price: ---");
+    else
+        mp_marketPriceLbl->setText("Market price: " + QString::number(marketPrice));
+
+    // Set the current spread
+    if (spread == -1.0) // if there aren't any orders of one of the types in the book
+        mp_spreadLbl->setText("Spread: ---");
+    else
+        mp_spreadLbl->setText("Spread: " + QString::number(spread));
+
+    this->repaint();
 }
